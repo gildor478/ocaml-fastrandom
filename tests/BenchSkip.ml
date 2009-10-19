7ed0f95eebdef595cdeb0ff63d6a680f3bada042
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2009-10-19 15:50:50 +0000

    Use a no buffer benchmark test

--- tests/BenchSkip.ml
+++ tests/BenchSkip.ml
@@ -16,15 +16,43 @@ let count =
 let ignore_int : int -> unit = ignore
 ;;
 
+let seed = 
+  [|
+    509760043; 399328820; 99941072; 112282318; 611886020; 516451399;
+    626288598; 337482183; 748548471; 808894867; 657927153; 386437385;
+    42355480; 977713532; 311548488; 13857891; 307938721; 93724463;
+    1041159001; 444711218; 1040610926; 233671814; 664494626; 1071756703;
+    188709089; 420289414; 969883075; 513442196; 275039308; 918830973;
+    598627151; 134083417; 823987070; 619204222; 81893604; 871834315;
+    398384680; 475117924; 520153386; 324637501; 38588599; 435158812;
+    168033706; 585877294; 328347186; 293179100; 671391820; 846150845;
+    283985689; 502873302; 718642511; 938465128; 962756406; 107944131;
+    192910970
+  |]
+;;
+
+let s =
+  FastRandom.State.make ~buffer:0 seed
+;;
+
 let res = 
   throughputN 
     3
     (
-      ["FastRandom.skip", 
+      [
+       "FastRandom.skip_no_buffer", 
        (fun () ->
           for i = 1 to count do
-            FastRandom.skip skip_count;
-            ignore_int (FastRandom.bits ())
+            FastRandom.State.skip s skip_count;
+            ignore_int (FastRandom.State.bits s)
+          done),
+       ();
+
+       "FastRandom.skip", 
+       (fun () ->
+          for i = 1 to count do
+            FastRandom.State.skip s skip_count;
+            ignore_int (FastRandom.State.bits s)
           done),
        ();
 
@@ -32,9 +60,9 @@ let res =
        (fun () ->
           for i = 1 to count do
             for j = 1 to skip_count do
-              ignore_int (FastRandom.bits ())
+              ignore_int (FastRandom.State.bits s)
             done;
-            ignore_int (FastRandom.bits ())
+            ignore_int (FastRandom.State.bits s)
           done),
        ();
 
